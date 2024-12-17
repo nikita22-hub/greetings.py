99279cd442468d636e518ac28b4adee739fd8707
nikita22-hub <nikiboy2021@gmail.com>
2024-12-17 23:24:28 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,16 @@
+username = 'Никита Зайцев'
+print(username)
+title='Заметки'
+print(title)
+content=('описание заметки')
+print(content)
+status=('статус заметки')
+print(status)
+created_date=('17-12-2024')
+print(created_date)
+created_date=('17.12')
+print(created_date)
+issue_date=('17-01-2025')
+print(issue_date)
+issue_date=('17.01')
+print(issue_date)
